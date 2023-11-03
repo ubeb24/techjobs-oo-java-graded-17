104764b494d6f27af237e3f88082fa41b8cad343
ubeb24 <saludadesj@gmail.com>
2023-11-02 21:55:16 -0400

    yay another task done, this time it is task 5 i think

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -31,6 +31,67 @@ public class Job {
     }
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
+//  if (id == int || name == null || name == "" || employer == null || employer == "" || location == null || location == "" || positionType == null || positionType == ""|| coreCompetency == null || coreCompetency == "") {
+//        return "Data not available";
+    //employer.getValue().isBlank() location.getValue().isBlank() positionType.getValue().isBlank() coreCompetency.getValue().isBlank()) {
+    //  return "Data not available";
+
+    //String stringFormatNoId = "\n" + "ID: " + "Data not available" + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" +
+    //                "Location: " + location + "\n" + "Position Type: " + positionType + "\n" + "Core Competency: " + coreCompetency + "\n";
+    //    return stringFormatNoId;
+    //               } else if (name.isBlank()) {
+    //            String stringFormatNoName = "\n" + "ID: " + id + "\n" + "Name: " + "Data not Available" + "\n" + "Employer: " + employer + "\n" +
+    //                    "Location: " + location + "\n" + "Position Type: " + positionType + "\n" + "Core Competency: " + coreCompetency + "\n";
+    //            return stringFormatNoName;
+    //        } else if (employer.getValue().isBlank()) {
+    //            String stringFormatNoEmployer = "\n" + "ID: " + id + "\n" + "Name: " + name + "\n" + "Employer: " + "Data not Available" + "\n" +
+    //                    "Location: " + location + "\n" + "Position Type: " + positionType + "\n" + "Core Competency: " + coreCompetency + "\n";
+    //            return stringFormatNoEmployer;
+    //        } else if (location.getValue().isBlank()) {
+    //            String stringFormatNoLocation = "\n" + "ID: " + id + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" +
+    //                    "Location: " + "Data not Available" + "\n" + "Position Type: " + positionType + "\n" + "Core Competency: " + coreCompetency + "\n";
+    //            return stringFormatNoLocation;
+    //        } else if (positionType.getValue().isBlank()) {
+    //            String stringFormatNoPositionType = "\n" + "ID: " + id + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" +
+    //                    "Location: " + location + "\n" + "Position Type: " + "Data not Available" + "\n" + "Core Competency: " + coreCompetency + "\n";
+    //            return stringFormatNoPositionType;
+    //        } else if (coreCompetency.getValue().isBlank()){
+    //            String stringFormatNoCoreCompetency = "\n" + "ID: " + id + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" +
+    //                    "Location: " + location + "\n" + "Position Type: " + positionType + "\n" + "Core Competency: " + "Data not Available" + "\n";
+    //            return stringFormatNoCoreCompetency;
+
+    //                   String stringFormat = "\n" + "ID: " + id + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" +
+//                    "Location: " + location + "\n" + "Position Type: " + positionType + "\n" + "Core Competency: " + coreCompetency + "\n";
+    @Override
+    public String toString() {
+         String IdFormat = "\n" + "ID: " + id;
+         String NameFormat = "\n" + "Name: " + name;
+         String EmployerFormat = "\n" + "Employer: " + employer;
+         String LocationFormat = "\n" + "Location: " + location;
+         String PTFormat = "\n" + "Position Type: " + positionType;
+         String CCFormat = "\n" + "Core Competency: " + coreCompetency;
+         if (id == 0) {
+             IdFormat = "\n" + "ID: " + "Data not available";
+         }
+         if (name.isBlank()) {
+             NameFormat = "\n" + "Name: " + "Data not available";
+         }
+         if (employer.getValue().isBlank()) {
+            EmployerFormat = "\n" + "Employer: " + "Data not available";
+        }
+         if (location.getValue().isBlank()) {
+          LocationFormat = "\n" + "Location: " + "Data not available";
+        }
+         if (positionType.getValue().isBlank()) {
+            PTFormat = "\n" + "Position Type: " + "Data not available";
+        }
+         if (coreCompetency.getValue().isBlank()){
+        CCFormat = "\n" + "Core Competency: " + "Data not available";
+        }
+               String stringFormat = IdFormat + NameFormat + EmployerFormat + LocationFormat + PTFormat + CCFormat +"\n";
+         return stringFormat;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -38,7 +99,6 @@ public class Job {
         Job job = (Job) o;
         return id == job.id && Objects.equals(name, job.name) && Objects.equals(employer, job.employer) && Objects.equals(location, job.location) && Objects.equals(positionType, job.positionType) && Objects.equals(coreCompetency, job.coreCompetency);
     }
-
 
     @Override
     public int hashCode() {
--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -4,10 +4,11 @@ package org.launchcode.techjobs.oo;
 //import java.lang.System.*;
 //import org.junit.jupiter.api.Test;
 
+import org.hamcrest.core.StringStartsWith;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
+
 //import static org.junit.jupiter.api.Assertions.*;
 public class JobTest {
     //TODO: Create your unit tests here
@@ -31,5 +32,25 @@ public class JobTest {
         Job test_job1 = new Job ("Super Silly Computer Salesman",new Employer("AFRC Federal"),new Location("New Jersey"),new PositionType("Super Sales"),new CoreCompetency("Gall"));
         Job test_job2 = new Job ("Super Silly Computer Salesman",new Employer("AFRC Federal"),new Location("New Jersey"),new PositionType("Super Sales"),new CoreCompetency("Gall"));
         assertNotEquals(test_job1, test_job2);
+    }
+    @Test
+    public void testToStringStartsAndEndsWithNewLine(){
+        Job test_job1 = new Job ("Super Silly Computer Salesman",new Employer("AFRC Federal"),new Location("New Jersey"),new PositionType("Super Sales"),new CoreCompetency("Gall"));
+   assertTrue(test_job1.toString().startsWith("\n"));
+    }
+    @Test
+    public void testToStringContainsCorrectLabelsAndData(){
+        Job test_job1 = new Job ("Super Silly Computer Salesman",new Employer("AFRC Federal"),new Location("New Jersey"),new PositionType("Super Sales"),new CoreCompetency("Gall"));
+   String string_test_job1 =  "\n" +"ID: " + 1 + "\n" + "Name: " + "Super Silly Computer Salesman" + "\n"+"Employer: " + "AFRC Federal" + "\n" +
+           "Location: " + "New Jersey" + "\n" + "Position Type: " + "Super Sales" + "\n" + "Core Competency: " + "Gall" + "\n";
+        assertEquals(test_job1.toString(), string_test_job1);
+    }
+
+   @Test
+   public void testToStringHandlesEmptyField(){
+       Job test_job2 = new Job ("Super Silly Computer Salesman",new Employer(""),new Location(""),new PositionType("Super Sales"),new CoreCompetency("Gall"));
+       String string_test_job2 =  "\n" +"ID: " + 1 + "\n" + "Name: " + "Super Silly Computer Salesman" + "\n"+"Employer: " + "Data not available" + "\n" +
+               "Location: " + "Data not available" + "\n" + "Position Type: " + "Super Sales" + "\n" + "Core Competency: " + "Gall" + "\n";
+       assertEquals(test_job2.toString(), string_test_job2);
     }
 }
